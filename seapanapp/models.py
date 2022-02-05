e45876f1ebaceaa3795146726f58ae52417045b3
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2022-02-05 18:20:53 +0000

    Basic link

--- seapanapp/models.py
+++ seapanapp/models.py
@@ -5,9 +5,10 @@ from django.db import models
 
 class Recording(models.Model):
     panopto_id = models.TextField()
+    name = models.TextField()
 
     def __str__(self):
-        return self.panopto_id[:8]
+        return f"{self.name} {self.panopto_id[:8]}"
 
 
 class QuestionAnswer(models.Model):
@@ -21,3 +22,12 @@ class QuestionAnswer(models.Model):
 
     def answer_start(self):
         return self.answer[:100]
+
+    def to_url(self):
+        t = (
+            self.timestamp.hour * 60 * 60
+            + self.timestamp.minute * 60
+            + self.timestamp.second
+        )
+        pid = self.recording.panopto_id
+        return f"https://imperial.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id={pid}&start={t}"
